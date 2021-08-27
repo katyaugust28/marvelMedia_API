4abdc39f4cab7d35347e57f73b4d28177872bab9
katyaugust28 <katyaugust28@gmail.com>
2021-08-26 20:13:59 -0700

    update functions within add a movie, delete a movie

--- index.js
+++ index.js
@@ -7,58 +7,72 @@ app.use(morgan('common'));
 
 let topMovies =[
   {
+    id: '1',
     title: 'Iron Man',
     year: '2008'
   },
   {
+    id: '2',
     title: 'Iron Man 2',
     year: '2010'
   },
   {
+    id: '3',
     title:'Thor',
     year: '2011'
   },
   {
+    id: '4',
     title: 'Captain America: The First Avenger',
     year: '2011'
   },
   {
+    id: '5',
     title: 'The Incredibel Hulk',
     year: '2008'
   },
   {
+    id: '6',
     title: 'The Avengers',
     year: '2012'
   },
   {
+    id: '7',
     title: 'Iron Man 3',
     year: '2013'
   },
   {
+    id: '8',
     title: 'Thor: The Dark World',
     year: '2013'
   },
   {
+    id: '9',
     title: 'Captain America: The Winter Soldier',
     year: '2014'
   },
   {
+    id: '10',
     title: 'Guardians of the Galaxy',
     year: '2014'
   },
   {
+    id: '11',
     title: 'Avengers: Age of Ultron',
     year: '2015'
   },
   {
+    id: '12',
     title: 'Ant-Man',
     year: '2015'
   },
   {
+    id: '13',
     title: 'Captain America: Civil War',
     year: '2016'
   },
   {
+    id: '14',
     title: 'Doctor Strange',
     year: '2016'
   }
@@ -78,23 +92,47 @@ app.get('/movies', (req, res) => {
 //Get data about a single movie
 app.get('/movies/:title', (req, res) => {
   res.json(topMovies.find((movie) => {
-    return movies.title === req.params.title;
+    return movie.title === req.params.title;
   }));
 });
 
-//Add a movie to their list
+//Add a movie
 app.post('/movies/', (req, res) => {
-  res.send('Successful POST request adding a new movie to the collection');
+  let newMovie= req.body;
+  if (!newMovie.title){
+    const message= 'Missing movie title in request body';
+    res.status(400).send(message);
+  } else {
+    newMovie.id=uuid.v4();
+    topMovies.push(newMovie);
+  }
 });
 
-//Remove a movie by ID to their list
+//Remove a movie by ID
 app.delete('/movies/:ID', (req, res) => {
-  res.send('Successful DELETE request removing a movie by using its ID');
+  let movie= topMovies.find((movie) => {
+    return movie.id === req.params.id;
+  })
+  if (movie) {
+    topMovies =topMovies.filter((obj) => {
+      return obj.id !== req.params.id});
+      res.status(201).send('Movie with the ID ' + req.params.id + ' was deleted.');
+  } else {
+    res.status(404).send('Movie with the ID ' + req.params.id + ' was not found.')
+  }
 });
 
-//Update the year of a movie by Title
+//Update the year of a movie by title
 app.put('/movies/:title/:year', (req, res) => {
-  res.send('Successful PUT request updating the year of a certain movie');
+  let movie = topMovies.find((movie) => {
+    return movie.title = req.params.title;
+  });
+  if (movie) {
+    movie.year = parseInt(req.params.year);
+    res.status(201).send('Movie ${req.params.title} was assigned the year of ${req.params.year}.');
+  } else {
+    res.status(404).send('Movie with the title '  + req.params.title + ' was not found.');
+  };
 });
 
 app.use(express.static('public'));
