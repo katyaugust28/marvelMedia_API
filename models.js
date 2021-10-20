96b25b02f3b96780ccf8c0684c5b748d9ef1ec2d
katyaugust28 <katyaugust28@gmail.com>
2021-10-19 17:54:07 -0700

    update models.js

--- models.js
+++ models.js
@@ -1,15 +1,18 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 //Defining the movie schema
-let movieSchema= mongoose.Schema({
-  Title: {type: String, required: true},
-  Description: {type: String, required: true},
-  Genre: {type: mongoose.Schema.Types.ObjectId, ref: 'Genre'},
+let movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String
+  },
   Director: {
-      Name: String,
-      Bio: String,
-      Birthyear: String
+    Name: String,
+    Bio: String,
+    Birthyear: String
   },
   Superheroes: [String],
   ImagePath: String,
@@ -17,16 +20,16 @@ let movieSchema= mongoose.Schema({
 });
 
 //Defining the user schema
-let userSchema= mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
+let userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
   Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
-userSchema.statics.hashPassword =(password) => {
-  return bcrypt.hashSync(password,10);
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
 };
 
 userSchema.methods.validatePassword = function (password) {
@@ -34,7 +37,7 @@ userSchema.methods.validatePassword = function (password) {
 };
 
 //Defining the Genre Schema
-let genreSchema= mongoose.Schema({
+let genreSchema = mongoose.Schema({
   Genre: {
     Name: String,
     Description: String
@@ -42,11 +45,11 @@ let genreSchema= mongoose.Schema({
 });
 
 //Creating the models
-let Movie= mongoose.model('Movie', movieSchema);
-let User= mongoose.model('User', userSchema);
-let Genre= mongoose.model('Genre', genreSchema);
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+let Genre = mongoose.model('Genre', genreSchema);
 
 //Exporting the models
-module.exports.Movie= Movie;
-module.exports.User= User;
-module.exports.Genre= Genre;
+module.exports.Movie = Movie;
+module.exports.User = User;
+module.exports.Genre = Genre;
